101c6ca82468e75d7db5b80f9955aa38b8426202
Vadimololo19 <vadyst.tkachev@yandex.ru>
2024-09-15 20:57:45 +0300

    Task 1.1 is ready for check.

--- Tasks1/Task1_1/task.c
+++ Tasks1/Task1_1/task.c
@@ -1,16 +1,44 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
-{
-	int x = 2.0;
-	float y = 0.7;
-	int z = -1;
 
-	double a = powl(x * y * z + abs(z * sin(y)), 1 / 3);
-	double b = y * cos(x * z * sin(y)) + 3;
+/**
+ * @brief функция для вычисления значения a.
+ * @param x Значение переменной x.
+ * @param y Значение переменной y.
+ * @param z Значение переменной z.
+ * @return Результат вычисления a.
+*/
+double get_a(double x, double y, double z);
 
-	printf("%f", a);
-	printf("\n");
-	printf("%f", b);
+/**
+ * @brief функция для вычисления значения b.
+ * @param x Значение переменной x.
+ * @param y Значение переменной y.
+ * @param z Значение переменной z.
+ * @return Результат вычисления b.
+*/
+double get_b(double x, double y, double z);
+
+/**
+ * @brief Точка входа в программу.
+ * @return Возвращает в случае успеха.
+*/
+int main() {
+	const double x = 2.0;
+	const double y = 0.7;
+	const double z = -1.0;
+
+	double a = get_a(x, y, z);
+	double b = get_b(x, y, z);
+
+	printf("a = %f \nb = %f", a, b);
+
+	return 0;
+}
+double get_a(double x, double y, double z) {
+	return cbrt(x * y * z + fabs(z * sin(y)));
+}
+double get_b(double x, double y, double z) {
+	return y * cos(x * z * sin(y)) + 3;
 }
