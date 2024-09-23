7e19a9d5f355a47f8e05b61325291c732f01267e
Vadimololo19 <vadyst.tkachev@yandex.ru>
2024-09-23 14:50:24 +0300

    rtr

---
+++ Task1_2/task.c
@@ -0,0 +1,55 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+/**
+* @brief Функция проверки введенных данных.
+* @return Возвращает значение, если правильное, закрывает программу, если иначе.
+*/
+
+double get_input();
+
+/**
+* @brief Функция перевода байтов в мегабайты.
+* @params bytes - байты, которые ввел пользователь.
+* @return Возвращает значение в Мегабайтах.
+*/
+double get_mb(double bytes);
+/**
+* @brief Функция перевода байтов в гигабайты.
+* @params bytes - байты, которые ввел пользователь.
+* @return Возвращает значение в Гигабайтах.
+*/
+double get_gb(double bytes);
+
+int main()
+{
+	puts("Введите значение в байтах");
+	double bytes = get_input(), mb = get_mb(bytes), gb = get_gb(bytes);
+	printf("mb = %lf\ngb = %lf", mb, gb);
+	return 0;
+}
+
+double get_input()
+{
+	double input;
+
+	if ((scanf_s("%lf", &input) != 1) || (input <= 0)) {
+		printf("Input error");
+		exit(EXIT_FAILURE);
+	}
+
+	return input;
+}
+
+double get_mb(double bytes)
+{
+	const double mb_value = 1048576;
+	return bytes / mb_value;
+}
+
+double get_gb(double bytes)
+{
+	const double gb_value = 1073741824;
+	return bytes / gb_value;
+}
