e7fe898592c84a1238ac4e92f280cbcf2bdb54a7
Vadimololo19 <vadyst.tkachev@yandex.ru>
2024-10-22 22:50:29 +0300

    Corrections

--- Task4_1/task.c
+++ Task4_1/task.c
@@ -7,33 +7,33 @@
 * @param command - переменная для выбора метода заполнения массива
 * @param array - массив, задаваемый пользователем
 * @param low_value - начало массива
-* @param high_value - размер массива
+* @param size_array - размер массива
 * @remarks при неправильном вводе команды выводит ошибку
 */
-void fill_array(const int command, int* array, int low_value, int high_value);
+void fill_array(const int command, int* array, int low_value, int size_array);
 
 /**
 * @brief Функция заполнения массива интервалом, который задал пользователь
 * @param array - массив, задаваемый пользователем
 * @param low_value - начало массива
-* @param high_value - размер массива
+* @param size_array - размер массива
 */
-void fill_by_input(int* array, int low_value, int high_value);
+void fill_by_input(int* array, int size_array);
 
 /**
 * @brief Функция заполнения массива случайными числами
 * @param array - массив, задаваемый пользователем
 * @param low_value - начало массива
-* @param high_value - размер массива
+* @param size_array - размер массива
 */
-void fill_by_random(int* array, int low_value, int high_value);
+void fill_by_random(int* array, int low_value, int size_array);
 
 /**
 * @brief Функция вывода массива
 * @param array - массив, задаваемый пользователем
-* @param high_value - размер массива
+* @param size_array - размер массива
 */
-void print_array(int* array, int high_value);
+void print_array(int* array, int size_array);
 
 /**
 * @brief Функция ввода значения пользователем
@@ -44,26 +44,26 @@ int input(void);
 /**
 * @brief Функция выполнения задания 1
 * @param array - массив, задаваемый пользователем
-* @param high_value - размер массива
+* @param size_array - размер массива
 * @return Возвращает сумму чисел согласно условию
 */
-int task1(int* arr, int high_value);
+int task1(int* arr, int size_array);
 
 /**
 * @brief Функция выполнения задания 2
 * @param array - массив, задаваемый пользователем
-* @param high_value - размер массива
+* @param size_array - размер массива
 * @remarks Выводит индексы согласно условию
 */
-void task2(int* arr, int high_value);
+void task2(int* arr, int size_array);
 
 /**
 * @brief Функция выполнения задания 3
 * @param array - массив, задаваемый пользователем
-* @param high_value - размер массива
+* @param size_array - размер массива
 * @remarks Выводит измененный массив
 */
-void task3(int* arr, int high_value);
+void task3(int* arr, int size_array);
 
 /**
 * @brief Точка входа
@@ -72,22 +72,19 @@ void task3(int* arr, int high_value);
 int main()
 {
 	puts("Enter command, low value, size of array");
-	int command = input(), low_value = input(), high_value = input() + 1, *arr;
-	arr = (int*)malloc(high_value * sizeof(int));
+	int command = input(), low_value = input(), size_array = input() + 1, *arr;
+	arr = (int*)malloc(size_array * sizeof(int));
 	if (arr == NULL) 
 	{
 		printf("Memory input");
 		exit(EXIT_FAILURE);
 	}
-	else 
-	{
-		fill_array(command, arr, low_value, high_value);
-		print_array(arr, high_value);
-		printf("task1: %d\n", task1(arr, high_value));
-		puts("task2: ");
-		task2(arr, high_value);
-		task3(arr, high_value);
-	}
+	fill_array(command, arr, low_value, size_array);
+	print_array(arr, size_array);
+	printf("task1: %d\n", task1(arr, size_array));
+	puts("task2: ");
+	task2(arr, size_array);
+	task3(arr, size_array);
 }
 
 /**
@@ -101,31 +98,29 @@ enum Task
 	fill_input,
 };
 
-void fill_array(const int command, int* array,int low_value,int high_value)
+void fill_array(const int command, int* array,int low_value,int size_array)
 {
 	switch ((enum Task)command)
 	{
-	case fill_random: fill_by_random(array, low_value, high_value); break;
-	case fill_input: fill_by_input(array,low_value,high_value); break;
+	case fill_random: fill_by_random(array, low_value, size_array); break;
+	case fill_input: fill_by_input(array, size_array); break;
 	default: puts("Command error"); exit(EXIT_FAILURE);
 	}
 }
 
-void fill_by_input(int* array, int low_value, int high_value)
+void fill_by_input(int* array, int size_array)
 {
-	int n = low_value;
-	for (int i = 0; i < high_value; i++)
+	for (int i = 0; i < size_array; i++)
 	{
-		array[i] = n;
-		n++;
+		array[i] = input();
 	}
 }
 
-void fill_by_random(int* array,int low_value, int high_value)
+void fill_by_random(int* array,int low_value, int size_array)
 {
-	for (int i = 0; i < high_value; i++)
+	for (int i = 0; i < size_array; i++)
 	{
-		array[i] = rand() % (high_value-low_value+1) + low_value;
+		array[i] = rand() % (size_array-low_value+1) + low_value;
 	}
 }
 
@@ -140,20 +135,20 @@ int input(void)
 	return input;
 }
 
-void print_array(int* array, int high_value)
+void print_array(int* array, int size_array)
 {
 	puts("[");
-	for (int i = 0; i < high_value; i++)
+	for (int i = 0; i < size_array; i++)
 	{
 		printf("%d,", array[i]);
 	}
 	puts("]");
 }
 
-int task1(int* arr,int high_value)
+int task1(int* arr,int size_array)
 {
 	int s = 0;
-	for (int i = 0; i < high_value; i++)
+	for (int i = 0; i < size_array; i++)
 	{
 		if (abs(arr[i]) < 10)
 		{
@@ -163,25 +158,25 @@ int task1(int* arr,int high_value)
 	return s;
 }
 
-void task2(int* arr, int high_value)
+void task2(int* arr, int size_array)
 {
-	for (int i = 0; i < high_value; i++)
+	for (int i = 0; i < size_array; i++)
 	{
-		if ((arr[i] > arr[i + 1]) && (i != NULL))
+		if ((arr[i] > arr[i + 1]) && (arr[i] != NULL))
 		{
 			printf("%d\n", i);
 		}
 	}
 }
 
-void task3(int* arr, int high_value)
+void task3(int* arr, int size_array)
 {
-	for (int i = 0; i < high_value; i++)
+	for (int i = 0; i < size_array; i++)
 	{
 		if (arr[i] % 3 == 0)
 		{
 			arr[i] *= arr[2];
 		}
 	}
-	print_array(arr, high_value);
+	print_array(arr, size_array);
 }
