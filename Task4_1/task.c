37093b9a18e0f719c7a951c3ba6f003f395dabc6
Vadimololo19 <vadyst.tkachev@yandex.ru>
2024-10-29 21:38:42 +0300

    Corrections

--- Task4_1/task.c
+++ Task4_1/task.c
@@ -1,6 +1,7 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 /**
 * @brief Функция выбора метода заполнения массива пользователем
@@ -10,7 +11,7 @@
 * @param size_array - размер массива
 * @remarks при неправильном вводе команды выводит ошибку
 */
-void fill_array(const int command, size_t* array, const int low_value, const size_t size_array);
+void fill_array(const int command, size_t* array, const size_t low_value, const size_t high_value, const size_t size_array);
 
 /**
 * @brief Функция заполнения массива интервалом, который задал пользователь
@@ -25,7 +26,7 @@ void fill_by_input(size_t* array, const size_t size_array);
 * @param low_value - начало массива
 * @param size_array - размер массива
 */
-void fill_by_random(size_t* array, const int low_value, const size_t size_array);
+void fill_by_random(size_t* array, const size_t low_value, const size_t high_value, const size_t size_array);
 
 /**
 * @brief Функция вывода массива
@@ -44,7 +45,7 @@ int input(void);
 * @brief Функция проверки введенного значени
 * @remarks если значение 0 и меньше возвращает код ошибки /c EXIT_FAILURE
 */
-void zero_input_check(void);
+size_t get_above_zero_value(void);
 
 /**
 * @brief Функция выполнения задания 1
@@ -84,36 +85,37 @@ enum Task
 /**
 * @brief Точка входа
 * @return Возвращает код ошибки 0
-*/ 
+*/
 int main(void)
 {
-	puts("Enter command, low value, size of array");
-	int command = input(), low_value = input();
-	size_t *arr;
-	size_t size_array = zero_input_check();
+	srand(time(NULL));
+	puts("Enter command,size of array, low and high value of array");
+	int command = input();
+	size_t* arr;
+	size_t size_array = get_above_zero_value(), low_value = get_above_zero_value(), high_value = get_above_zero_value();
 	arr = (size_t*)malloc(size_array * sizeof(size_t));
-	if (arr == NULL) 
+	if (arr == NULL)
 	{
 		printf("Memory input");
 		exit(EXIT_FAILURE);
 	}
-	fill_array(command, arr, low_value, size_array);
+	fill_array(command, arr, low_value, high_value, size_array);
 	print_array(arr, size_array);
 	printf("task1: %d\n", task1(arr, size_array));
 	puts("task2: ");
 	task2(arr, size_array);
 	puts("task3: ");
 	task3(arr, size_array);
-	exit(*arr);
+	free(arr);
 
 	return 0;
 }
 
-void fill_array(const int command, size_t* array,const int low_value,const size_t size_array)
+void fill_array(const int command, size_t* array, const size_t low_value, const size_t high_value, const size_t size_array)
 {
 	switch ((enum Task)command)
 	{
-	case fill_random: fill_by_random(array, low_value, size_array); break;
+	case fill_random: fill_by_random(array, low_value, high_value, size_array); break;
 	case fill_input: fill_by_input(array, size_array); break;
 	default: puts("Command error"); exit(EXIT_FAILURE);
 	}
@@ -127,18 +129,18 @@ void fill_by_input(size_t* array, const size_t size_array)
 	}
 }
 
-void fill_by_random(size_t* array,const int low_value, const size_t size_array)
+void fill_by_random(size_t* array, const size_t low_value, const size_t high_value, const size_t size_array)
 {
 	for (int i = 0; i < size_array; i++)
 	{
-		array[i] = rand() % (size_array-low_value+1) + low_value;
+		array[i] = rand() % (high_value - low_value + 1) + low_value;
 	}
 }
 
 int input(void)
 {
 	int input = 0;
-	if (scanf_s("%d", &input) != 1)
+	if (scanf("%d", &input) != 1)
 	{
 		puts("Input error");
 		exit(EXIT_FAILURE);
@@ -146,7 +148,7 @@ int input(void)
 	return input;
 }
 
-size_t zero_input_check(void)
+size_t get_above_zero_value(void)
 {
 	int input_ = input();
 	if (input <= 0)
@@ -157,7 +159,7 @@ size_t zero_input_check(void)
 	return input_;
 }
 
-void print_array(const size_t* array,const size_t size_array)
+void print_array(const size_t* array, const size_t size_array)
 {
 	puts("[");
 	for (int i = 0; i < size_array; i++)
@@ -167,7 +169,7 @@ void print_array(const size_t* array,const size_t size_array)
 	puts("]");
 }
 
-int task1(const size_t* arr,const size_t size_array)
+int task1(const size_t* arr, const size_t size_array)
 {
 	int s = 0;
 	for (int i = 0; i < size_array; i++)
@@ -182,9 +184,9 @@ int task1(const size_t* arr,const size_t size_array)
 
 void task2(const size_t* arr, const size_t size_array)
 {
-	for (int i = 0; i < size_array; i++)
+	for (int i = 0; i < size_array - 1; i++)
 	{
-		if ((arr[i] > arr[i + 1]) && (arr[i + 1] != NULL))
+		if ((arr[i] > arr[i + 1]))
 		{
 			printf("%d\n", i);
 		}
@@ -202,3 +204,4 @@ void task3(size_t* arr, const size_t size_array)
 	}
 	print_array(arr, size_array);
 }
+
